03ec51a2f638b4a775b2e2eb6f767de01d9728df
AxolotlGamer8 <matthewaccomazzo@gmail.com>
2024-11-13 19:37:29 +0000

    Copied basis for login screen and spacecraft

--- spacecraft/App.tsx
+++ spacecraft/App.tsx
@@ -1,20 +1,105 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TextInput,
+  TouchableHighlight,
+  View,
+} from 'react-native';
 
 export default function App() {
+  const [email, setEmail] = React.useState('Email');
+  const [password, setpassword] = React.useState('Password');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.top}>
+        <Text style={styles.Title}>SpaceCraft</Text>
+      </View>
+      <View style={styles.Bottom}>
+        <TextInput
+          style={styles.textinput1}
+          value={email}
+          onChangeText={setEmail}
+          clearTextOnFocus={true}
+        />
+        <TextInput
+          style={styles.textinput2}
+          value={password}
+          onChangeText={setpassword}
+          clearTextOnFocus={true}
+        />
+        <TouchableHighlight
+          style={styles.Bcontainer}
+          onPress={() => {
+            alert('User ' + email + ' logged in successfully!');
+          }}>
+          <View style={styles.button}>
+            <Text style={styles.buttontext}>Login</Text>
+          </View>
+        </TouchableHighlight>
+        <Text style={styles.invisible}>Read Terms and Conditions</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  top: {
+    borderColor: 'black',
+    borderWidth: 4,
+    fontSize: 18,
+    backgroundColor: '#6a5ae8',
+    flex: 2,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  Title: {
+    fontSize: 45,
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  Bcontainer: {
+    marginBottom: 25,
+  },
+  invisible: {
+    color: '#d8d8d8',
+  },
+  button: {
+    width: 200,
+    height: 50,
+    backgroundColor: '#6a5ae8',
+    borderWidth: 2,
+    borderColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttontext: {
+    color: 'white',
+    fontSize: 20,
+  },
+  Bottom: {
+    flex: 5,
+    alignItems: 'center',
+    borderLeftWidth: 4,
+    borderRightWidth: 4,
+    borderBottomWidth: 4,
+  },
+  textinput1: {
+    borderWidth: 1,
+    width: 300,
+    height: 50,
+    marginTop: 65,
+    marginBottom: 25,
+  },
+  textinput2: {
+    borderWidth: 1,
+    width: 300,
+    height: 50,
+    marginBottom: 30,
   },
 });
