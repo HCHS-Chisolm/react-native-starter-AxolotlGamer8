daa4ab91ba5ef29e123a1a3f97f2034cf6475d68
AxolotlGamer8 <matthewaccomazzo@gmail.com>
2024-11-19 19:42:43 +0000

    added vital header info and cleaned a few things

--- spacecraft/App.tsx
+++ spacecraft/App.tsx
@@ -1,14 +1,16 @@
+// App.tsx
 import React from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
 
-import LoginScreen from './SCR/Screens/LoginScreen';
+import LoginScreen from './src/screens/LoginScreen';
 
 function App() {
-    return (
-        <PaperProvider>
-            <LoginScreen />
-        </PaperProvider>
-    );
+  return (
+    <PaperProvider>
+      <LoginScreen />
+    </PaperProvider>
+  );
 };
 
+// eslint-disable-next-line import/no-default-export
 export default App;
