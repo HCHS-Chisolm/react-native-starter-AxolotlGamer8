47c4a05c4e9da6d7f72f3c0125619f5f4715c3e3
AxolotlGamer8 <matthewaccomazzo@gmail.com>
2024-12-10 18:02:38 +0000

    Add content to lint and prettier

--- spacecraft/.eslintrc.js
+++ spacecraft/.eslintrc.js
@@ -1,4 +1,27 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: 'expo',
+  env: {
+    node: true
+  },
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  root: true, // make sure eslint picks up the config at the root of the directory
+  extends: ["expo",
+     "prettier",
+    "eslint:recommended", // ESLint rules
+    "plugin:@typescript-eslint/recommended", // TypeScript rules
+    "plugin:react/recommended", // React rules
+    "plugin:react/jsx-runtime", // support for React 17 JSX
+    "plugin:prettier/recommended" // Prettier recommended rules 
+    ],
+  plugins: ["prettier", "react", "react-native"],
+  rules: {
+    "prettier/prettier": "warn",
+    "react-native/no-color-literals": 2, // enforce color literals are not used
+    "react-native/no-unused-styles": 2, // detect unused StyleSheet rules
+    "react-native/no-raw-text": 0, // detect raw text outside of Text component
+    "react-native/sort-styles": 2, // enforce style definitions are sorted
+    {
+      usePrettierrc: true
+    }
+  },
 };
