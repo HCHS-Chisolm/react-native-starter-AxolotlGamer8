96a87fd957bf0369d7e48852c9a02c9f3766cc9a
AxolotlGamer8 <matthewaccomazzo@gmail.com>
2024-12-10 18:26:03 +0000

    Add more to lint

--- spacecraft/.eslintrc.js
+++ spacecraft/.eslintrc.js
@@ -13,15 +13,35 @@ module.exports = {
     "plugin:react/jsx-runtime", // support for React 17 JSX
     "plugin:prettier/recommended" // Prettier recommended rules 
     ],
-  plugins: ["prettier", "react", "react-native"],
-  rules: {
-    "prettier/prettier": "warn",
+  "rules": {
+    plugins: ["prettier", "react", "react-native", "eslint-plugin-simple-import-sort"],
     "react-native/no-color-literals": 2, // enforce color literals are not used
     "react-native/no-unused-styles": 2, // detect unused StyleSheet rules
     "react-native/no-raw-text": 0, // detect raw text outside of Text component
     "react-native/sort-styles": 2, // enforce style definitions are sorted
+    "simple-import-sort/exports": "warn", // enforce sorting exports within module
+    "simple-import-sort/imports": [
+      "warn",
+      {
+        groups: [
+          // Side effect imports.
+          ["^\\u0000"],
+          // Packages `react` related packages come first.
+          ["^react", "^@?\\w"],
+          // Environment variables
+          ["^(@env)(/.*|$)"],
+          // Parent imports. Put `..` last.
+          ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+          // Other relative imports. Put same-folder imports and `.` last.
+          ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+        ]
+  },
+  rules: {
+    "prettier/prettier": [ "warn",
+
     {
       usePrettierrc: true
     }
+  ]
   },
 };
